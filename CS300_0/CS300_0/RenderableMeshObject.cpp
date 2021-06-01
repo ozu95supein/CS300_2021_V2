7ddb1dd053646ec1ade9d0a5c224b5b4fe4eec3c
ozu <oscar.s@digipen.edu>
2021-06-01 11:35:23 +0200

    RenderableObject is being made

--- CS300_0/CS300_0/RenderableMeshObject.cpp
+++ CS300_0/CS300_0/RenderableMeshObject.cpp
@@ -1,7 +1,12 @@
 #include "RenderableMeshObject.h"
-RenderableMeshObject::RenderableMeshObject()
+RenderableMeshObject::RenderableMeshObject() : mObjectVBO ( 0 )
 {
-
+    mObjectVBO                  = 0;
+    mObjectVAO                  = 0;
+    mObjectNormal_VBO           = 0;
+    mObjectNormal_VAO           = 0;
+    mObjectAveragedNormal_VBO   = 0;
+    mObjectAveragedNormal_VAO   = 0;
 }
 RenderableMeshObject::RenderableMeshObject(MeshType t, int slices, GLuint& vbo, GLuint& vao, Mesh& mesh, GLuint& Normal_vbo, GLuint& Normal_vao, GLuint& Averaged_vbo, GLuint& Averaged_vao)
 {
@@ -30,10 +35,73 @@ RenderableMeshObject::RenderableMeshObject(MeshType t, int slices, GLuint& vbo, 
     mObjectNormal_VAO = Normal_vao;
     mObjectAveragedNormal_VBO = Averaged_vbo;
     mObjectAveragedNormal_VAO = Averaged_vao;
-
+    //generateNormal lines for the Mesh Object
     mObjectMesh.GenerateNormalLines();
     mObjectMesh.GenerateAveragedNormalLines();
-    InitializeMeshBuffers(mObjectVBO, mObjectVAO, mObjectMesh);
-    InitializeNormalBuffers(mObjectNormal_VBO, mObjectNormal_VAO, mObjectMesh);
-    InitializeAveragedNormalBuffers(mObjectAveragedNormal_VBO, mObjectAveragedNormal_VAO, mObjectMesh);
+    //initialize buffers for vbo and vaos
+    Renderable_InitAllBuffers();
+}
+
+
+void RenderableMeshObject::Renderable_InitAllBuffers()
+{
+    Renderable_InitializeMeshBuffers(mObjectVBO, mObjectVAO, mObjectMesh);
+    Renderable_InitializeNormalBuffers(mObjectNormal_VBO, mObjectNormal_VAO, mObjectMesh);
+    Renderable_InitializeAveragedNormalBuffers(mObjectAveragedNormal_VBO, mObjectAveragedNormal_VAO, mObjectMesh);
+}
+void RenderableMeshObject::Renderable_InitializeMeshBuffers(GLuint vbo, GLuint vao, Mesh& mesh)
+{
+    unsigned long stride = sizeof(Vertex);
+    // create buffer for VAO
+    glGenVertexArrays(1, &vao);
+    // create buffer for VBO
+    glGenBuffers(1, &vbo);
+    //bind so we are now doing stuff to the vao
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, (sizeof(Vertex) * mesh.GetVertexNum()), mesh.GetVertices(), GL_STATIC_DRAW);
+
+    //positions
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
+    //normals
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(offsetof(Vertex, normal)));
+    //uv
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(offsetof(Vertex, UV)));
+
+    // Unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+void RenderableMeshObject::Renderable_InitializeNormalBuffers(GLuint vbo, GLuint vao, Mesh& mesh)
+{
+    unsigned long stride = sizeof(glm::vec4);
+    // create buffer for VAO
+    glGenVertexArrays(1, &vao);
+    // create buffer for VBO
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, (sizeof(Mesh::NormalLine) * mesh.GetNormalsNum()), mesh.GetNormals(), GL_STATIC_DRAW);
+
+    //start
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
+}
+void RenderableMeshObject::Renderable_InitializeAveragedNormalBuffers(GLuint vbo, GLuint vao, Mesh& mesh)
+{
+    unsigned long stride = sizeof(glm::vec4);
+    // create buffer for VAO
+    glGenVertexArrays(1, &vao);
+    // create buffer for VBO
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, (sizeof(Mesh::NormalLine) * mesh.GetNormalsNum()), mesh.GetAveragedNormals(), GL_STATIC_DRAW);
+
+    //start
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
 }
--- CS300_0/CS300_0/RenderableMeshObject.h
+++ CS300_0/CS300_0/RenderableMeshObject.h
@@ -8,75 +8,25 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-void InitializeMeshBuffers(GLuint& vbo, GLuint& vao, Mesh& mesh)
-{
-    unsigned long stride = sizeof(Vertex);
-    // create buffer for VAO
-    glGenVertexArrays(1, &vao);
-    // create buffer for VBO
-    glGenBuffers(1, &vbo);
-    //bind so we are now doing stuff to the vao
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, (sizeof(Vertex) * mesh.GetVertexNum()), mesh.GetVertices(), GL_STATIC_DRAW);
 
-    //positions
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
-    //normals
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(offsetof(Vertex, normal)));
-    //uv
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, reinterpret_cast<void*>(offsetof(Vertex, UV)));
-
-    // Unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-}
-
-void InitializeNormalBuffers(GLuint& vbo, GLuint& vao, Mesh& mesh)
-{
-    unsigned long stride = sizeof(glm::vec4);
-    // create buffer for VAO
-    glGenVertexArrays(1, &vao);
-    // create buffer for VBO
-    glGenBuffers(1, &vbo);
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, (sizeof(Mesh::NormalLine) * mesh.GetNormalsNum()), mesh.GetNormals(), GL_STATIC_DRAW);
-
-    //start
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
-}
-void InitializeAveragedNormalBuffers(GLuint& vbo, GLuint& vao, Mesh& mesh)
-{
-    unsigned long stride = sizeof(glm::vec4);
-    // create buffer for VAO
-    glGenVertexArrays(1, &vao);
-    // create buffer for VBO
-    glGenBuffers(1, &vbo);
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, (sizeof(Mesh::NormalLine) * mesh.GetNormalsNum()), mesh.GetAveragedNormals(), GL_STATIC_DRAW);
-
-    //start
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, stride, 0);
-}
 class RenderableMeshObject
 {
 public:
     RenderableMeshObject();
     RenderableMeshObject(MeshType t, int slices, GLuint& vbo, GLuint& vao, Mesh& mesh, GLuint& Normal_vbo, GLuint& Normal_vao, GLuint& Averaged_vbo, GLuint& Averaged_vao);
+    
+    void Renderable_InitAllBuffers();
+    void Renderable_InitializeMeshBuffers(GLuint vbo, GLuint vao, Mesh & mesh);
+    void Renderable_InitializeNormalBuffers(GLuint vbo, GLuint vao, Mesh & mesh);
+    void Renderable_InitializeAveragedNormalBuffers(GLuint vbo, GLuint vao, Mesh & mesh);
+
 private:
 
     Mesh mObjectMesh;
-    GLuint& mObjectVBO;
-    GLuint& mObjectVAO;
-    GLuint& mObjectNormal_VBO;
-    GLuint& mObjectNormal_VAO;
-    GLuint& mObjectAveragedNormal_VBO;
-    GLuint& mObjectAveragedNormal_VAO;
+    GLuint mObjectVBO;
+    GLuint mObjectVAO;
+    GLuint mObjectNormal_VBO;
+    GLuint mObjectNormal_VAO;
+    GLuint mObjectAveragedNormal_VBO;
+    GLuint mObjectAveragedNormal_VAO;
 };
